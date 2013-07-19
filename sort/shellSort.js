30ca06c5ba0860ef28b3750fa649afc019b7b91a
NoahZhang <dguo_03@hotmail.com>
2013-07-19 13:45:35 +0800

    adjust indent

--- sort/shellSort.js
+++ sort/shellSort.js
@@ -1,25 +1,26 @@
 var shellSort;
   		
 shellSort = function(arr){
-    var n, tmp, i, j, increment;
+  var n, tmp, i, j, increment;
 				
-		n = arr.length;
-		increment = n;
+  n = arr.length;
+  increment = n;
 				
-		do {
-			   increment = parseInt(increment/3) + 1;
-					
-				 for(i = increment; i < n; i++){
-				   if(arr[i] < arr[i - increment]) {
-							tmp = arr[i];
+  do {
+       increment = parseInt(increment/3) + 1;
+			
+       for(i = increment; i < n; i++){
+         if(arr[i] < arr[i - increment]) {
+	   tmp = arr[i];
 	
-							for(j = i - increment; j >= 0 && tmp < arr[j]; j -= increment)
-								arr[j + increment] = arr[j];
+	   for(j = i - increment; j >= 0 && tmp < arr[j]; j -= increment) {
+	     arr[j + increment] = arr[j];
+	   }
 							
-						 	arr[j + increment] = tmp;
-						}
-				 }
-			} while(increment > 1);
+	   arr[j + increment] = tmp;
+         }
+       }
+  } while(increment > 1);
 				
-    return arr;
+  return arr;
 }
