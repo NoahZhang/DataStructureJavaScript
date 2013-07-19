5821bb6cb868e69eb14b5a0dc39fec6547de7729
NoahZhang <dguo_03@hotmail.com>
2013-07-19 13:21:49 +0800

    Update indent

--- shellSort.js
+++ shellSort.js
@@ -7,18 +7,18 @@ shellSort = function(arr){
 		increment = n;
 				
 		do {
-			     increment = parseInt(increment/3) + 1;
+			   increment = parseInt(increment/3) + 1;
 					
-					 for(i = increment; i < n; i++){
-						 if(arr[i] < arr[i - increment]){
-							 tmp = arr[i];
+				 for(i = increment; i < n; i++){
+				   if(arr[i] < arr[i - increment]){
+							tmp = arr[i];
 	
-							 for(j = i - increment; j >= 0 && tmp < arr[j]; j -= increment)
-								 arr[j + increment] = arr[j];
+							for(j = i - increment; j >= 0 && tmp < arr[j]; j -= increment)
+								arr[j + increment] = arr[j];
 							
-						 	 arr[j + increment] = tmp;
-						 }
-					 }
+						 	arr[j + increment] = tmp;
+						}
+				 }
 			} while(increment > 1);
 				
     return arr;
